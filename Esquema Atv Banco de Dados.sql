a9b7f1e82a68ff5df9ce4288257ff99d2ad3ac13
Coldrenatinho <araujorenato045@gmail.com>
2023-08-26 23:16:57 -0400

    Atividade03

--- Esquema Atv Banco de Dados.sql
+++ Esquema Atv Banco de Dados.sql
@@ -3,40 +3,18 @@
 #UNEMAT - UNIVERSIDADE DO ESTADO DO MATO GROSSO
 #https://github.com/coldrenatinho/
 #@coldrenatinho
-
-
-
-/*
-
-#TESTE
-
-CREATE DATABASE IF NOT EXISTS Atividade01;
-
-USE Atividade01;
-
-CREATE TABLE IF NOT EXISTS Cliente(
-
-    CodCliente INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    Nome Varchar(200) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS MunicipioCliente(
-    CodMunicipio INT PRIMARY KEY NOT NULL,
-    CodCliente  INT
-);
-#Pedimos a alteração da tabela que vamos realizar crial a constraint <ALTER TABLE> {NOME TABELA} <ADD CONSTRAINT foreing key > {NOME DA CONTRAINT} ({Nome da tupla a ser referenciada}) <REFERENCES> {Nome da table de referencia "Primary key "};
- ALTER TABLE MunicipioCliente ADD constraint foreign key FK_Cliente_CodCliente (CodCliente) references Cliente (CodCliente);
  
- */
- 
- 
- #-----------------------------------------------------------------------------------------
- #Atividade 02
-
+#-----------------------INICIO ATIVIDADE 02-----------------------
+#DROP DATABASE Atividade02; #REALIZAR TESTE DO SCRIP NO TERMINAL
+#----------------------------------------------
+#Novo Banco Atividade02
+#----------------------------------------------
  CREATE DATABASE IF NOT EXISTS Atividade02;
 
  USE Atividade02;
-
+#----------------------------------------------
+#Tabela Paciente
+#----------------------------------------------
 CREATE TABLE IF NOT EXISTS Paciente(
     CodPaciente INT PRIMARY KEY AUTO_INCREMENT,
     Nome Char(200),
@@ -53,6 +31,9 @@ CREATE TABLE IF NOT EXISTS Paciente(
     CodMatriculConvenio int NOT NULL
 );
 
+#----------------------------------------------
+#Tabela Medico
+#----------------------------------------------
 CREATE TABLE IF NOT EXISTS Medico(
     CodMedico int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     Nome CHAR(200) NOT NULL,
@@ -67,6 +48,9 @@ CREATE TABLE IF NOT EXISTS Medico(
     Bairro CHAR(200) NOT NULL
 );
 
+#----------------------------------------------
+#Tabela Horario Medico
+#----------------------------------------------
 CREATE TABLE IF NOT EXISTS HorarioMedico(
     CodHorario INT PRIMARY KEY NOT NULL, #GeraLista de Horarios 
     CodMedico INT NOT NULL UNIQUE, #VICULA O HORÁRIO AO MÉDICO
@@ -78,8 +62,14 @@ CREATE TABLE IF NOT EXISTS HorarioMedico(
     DataSaida TIME NOT NULL
 );
 
+#----------------------------------------------
+#Adição restrição por chave estrangeira na tabela HorarioMedico
+#----------------------------------------------
 ALTER TABLE HorarioMedico ADD constraint foreign key FK_Medico_CodMedico (CodMedico) REFERENCES Medico (CodMedico);
 
+#----------------------------------------------
+#Tabela Consulta
+#----------------------------------------------
 CREATE TABLE IF NOT EXISTS Consulta(
     CodConsulta INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     CodPaciente INT NOT NULL,
@@ -88,46 +78,15 @@ CREATE TABLE IF NOT EXISTS Consulta(
     StatusConsulta CHAR(200) # Atendido/Cancelada/Aberta
 );
 
+#----------------------------------------------
+#Adição restrição por chave estrangeira na tabela Consutla
+#----------------------------------------------
 ALTER TABLE Consulta ADD constraint foreign key FK_Medico_CodMedico (CodMedico) REFERENCES Medico (CodMedico);
 ALTER TABLE Consulta ADD constraint foreign key FK_Pacinte_CodPacinte (CodPaciente) REFERENCES Paciente (CodPaciente);
-#-----------------------------------------------------------------------------------------
-#Gerando um insert nas Tabelas Criadas acima
-#-----------------------------------------------------------------------------------------
-INSERT INTO Paciente (Nome, RG, CPF, Sexo, DataNascimento, Logradouro, NumeroLogradouro, CEP, Bairro, Celular, NomeConvernio, CodMatriculConvenio)
-VALUES
-    ('Maria Silva', 12345678, 98765432, 'F', '1990-05-15', 'Rua A', 123, 12345678, 'Centro', 987654321, 'ConvenioSaude', 456),
-    ('João Santos', 87654321, 54321678, 'M', '1985-10-20', 'Av. B', 456, 87654321, 'Bairro1', 987654321, 'ConvenioMed', 789),
-    ('Ana Oliveira', 23456789, 87654321, 'F', '1988-02-28', 'Rua B', 456, 23456789, 'Bairro2', 876543210, 'PlanoVida', 123),
-    ('Pedro Almeida', 34567890, 76543210, 'M', '1995-11-10', 'Av. C', 789, 34567890, 'Centro', 765432109, 'ConvenioSaude', 234);
 
-INSERT INTO Medico (Nome, CRM, Especialidade, CodHorario, Celular, CelularRecado, Logradouro, NumeroLogradouro, CEP, Bairro)
-VALUES
-    ('Dr. Ana Souza', 123456, 'Cardiologia', 1, 987654321, 987654322, 'Rua X', 789, 98765432, 'Centro'),
-    ('Dr. Carlos Santos', 987654, 'Dermatologia', 2, 987654321, 987654322, 'Av. Y', 654, 87654321, 'Bairro2'),
-    ('Dra. Luiza Rodrigues', 567890, 'Pediatria', 3, 876543210, 876543211, 'Rua Y', 987, 56789012, 'Bairro3'),
-    ('Dr. Marcelo Lima', 678901, 'Ortopedia', 4, 765432109, 765432210, 'Av. Z', 123, 45678901, 'Centro');
-
-INSERT INTO HorarioMedico (CodHorario, CodMedico, DataInicio, DataFim, DiaSemanaInicio, DiaSemanaFim, HoraEtranda, DataSaida)
-VALUES
-    (1, 1, '2023-09-01', '2023-12-31', 1, 5, '08:00:00', '17:00:00'),
-    (2, 2, '2023-09-01', '2023-12-31', 1, 5, '09:00:00', '18:00:00'),
-    (3, 3, '2023-09-01', '2023-12-31', 1, 3, '10:00:00', '15:00:00'),
-    (4, 4, '2023-09-01', '2023-12-31', 3, 5, '12:00:00', '20:00:00');
-
-INSERT INTO Consulta (CodPaciente, CodMedico, DiaConsulta, StatusConsulta)
-VALUES
-    (1, 1, '2023-09-10 10:00:00', 'Atendido'),
-    (2, 2, '2023-09-12 14:30:00', 'Aberta'),
-    (1, 3, '2023-09-15 11:30:00', 'Atendido'),
-    (3, 4, '2023-09-18 14:00:00', 'Aberta');
-
-#-----------------------FIM DO INSERT----------------------- 
-system cls #Limpa a tela
-\! echo 'Ficha de atendimentos'; #Faz o print NO TERMINAL
-
-
-#-----------------------INSERINDO VIEW-----------------------
-
+#----------------------------------------------
+#Adição VIEW ConsultasAgendadas
+#----------------------------------------------
 CREATE OR REPLACE VIEW ConsultaAgendada AS
     SELECT c.CodConsulta, m.Nome AS 'Nome Medico', m.Especialidade as 'Especialide', p.Nome AS 'Nome Pacinte', c.DiaConsulta as 'Data Consulta', c.StatusConsulta as 'Status da Consulta'
     FROM Consulta C
@@ -136,27 +95,67 @@ CREATE OR REPLACE VIEW ConsultaAgendada AS
     INNER JOIN Paciente p on 
     p.CodPaciente = c.CodPaciente;
 
-#-----------------------INSERINDO VIEW-----------------------
+#-----------------------GERANDO INSERT NO BANCO 'Atividade02'-----------------------
 
+#----------------------------------------------
+#Adição tabela Paciente
+#----------------------------------------------
+INSERT INTO Paciente (Nome, RG, CPF, Sexo, DataNascimento, Logradouro, NumeroLogradouro, CEP, Bairro, Celular, NomeConvernio, CodMatriculConvenio)
+VALUES
+    ('Maria Silva', 12345678, 98765432, 'F', '1990-05-15', 'Rua A', 123, 12345678, 'Centro', 987654321, 'ConvenioSaude', 456),
+    ('João Santos', 87654321, 54321678, 'M', '1985-10-20', 'Av. B', 456, 87654321, 'Bairro1', 987654321, 'ConvenioMed', 789),
+    ('Ana Oliveira', 23456789, 87654321, 'F', '1988-02-28', 'Rua B', 456, 23456789, 'Bairro2', 876543210, 'PlanoVida', 123),
+    ('Pedro Almeida', 34567890, 76543210, 'M', '1995-11-10', 'Av. C', 789, 34567890, 'Centro', 765432109, 'ConvenioSaude', 234);
 
+#----------------------------------------------
+#Adição tabela Medico
+#----------------------------------------------
+INSERT INTO Medico (Nome, CRM, Especialidade, CodHorario, Celular, CelularRecado, Logradouro, NumeroLogradouro, CEP, Bairro)
+VALUES
+    ('Dr. Ana Souza', 123456, 'Cardiologia', 1, 987654321, 987654322, 'Rua X', 789, 98765432, 'Centro'),
+    ('Dr. Carlos Santos', 987654, 'Dermatologia', 2, 987654321, 987654322, 'Av. Y', 654, 87654321, 'Bairro2'),
+    ('Dra. Luiza Rodrigues', 567890, 'Pediatria', 3, 876543210, 876543211, 'Rua Y', 987, 56789012, 'Bairro3'),
+    ('Dr. Marcelo Lima', 678901, 'Ortopedia', 4, 765432109, 765432210, 'Av. Z', 123, 45678901, 'Centro');
+
+#----------------------------------------------
+#Adição tabela HorarioMedico
+#----------------------------------------------
+INSERT INTO HorarioMedico (CodHorario, CodMedico, DataInicio, DataFim, DiaSemanaInicio, DiaSemanaFim, HoraEtranda, DataSaida)
+VALUES
+    (1, 1, '2023-09-01', '2023-12-31', 1, 5, '08:00:00', '17:00:00'),
+    (2, 2, '2023-09-01', '2023-12-31', 1, 5, '09:00:00', '18:00:00'),
+    (3, 3, '2023-09-01', '2023-12-31', 1, 3, '10:00:00', '15:00:00'),
+    (4, 4, '2023-09-01', '2023-12-31', 3, 5, '12:00:00', '20:00:00');
+
+#----------------------------------------------
+#Adição tabela Consutla
+#----------------------------------------------
+INSERT INTO Consulta (CodPaciente, CodMedico, DiaConsulta, StatusConsulta)
+VALUES
+    (1, 1, '2023-09-10 10:00:00', 'Atendido'),
+    (2, 2, '2023-09-12 14:30:00', 'Aberta'),
+    (1, 3, '2023-09-15 11:30:00', 'Atendido'),
+    (3, 4, '2023-09-18 14:00:00', 'Aberta');
+
+#-----------------------FIM DO INSERT-----------------------
+
+#-----------------------LIMPEZA DO TERMINAL----------------------- 
+system cls #Limpa a tela terminal
+\! echo 'Ficha de atendimentos'; #Faz o print no terminal
 
 #-----------------------Retorno da View-----------------------
 SELECT *
 FROM ConsultaAgendada;
-#-----------------------Retorno da View-----------------------
+
 #-----------------------FIM DA ATIVIDADE-----------------------
 
 
 #-----------------------INICIO ATIVIDADE 03-----------------------
-
-#----------------------------------------------
-#Atividade03
-#----------------------------------------------
+#DROP DATABASE Atividade03; #REALIZAR TESTE DO SCRIP NO TERMINAL
 #----------------------------------------------
 #Novo Banco Atividade03
 #----------------------------------------------
 CREATE DATABASE IF NOT EXISTS Atividade03;
-
 USE Atividade03;
 
 #----------------------------------------------
@@ -190,7 +189,7 @@ ALTER TABLE TelefoneInstrutor ADD CONSTRAINT FOREIGN KEY FK_Instrutor_CodInstrut
 #----------------------------------------------
 CREATE TABLE IF NOT EXISTS Aluno(
     CodAluno INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    CodTurma int NOT NULL,
+    CodTurma INT NOT NULL,
     DataMatricula DATE NOT NULL,
     Nome VARCHAR(200) NOT NULL,
     Logradouro VARCHAR(200) NOT NULL,
@@ -224,13 +223,39 @@ CREATE IF NOT EXISTS Turma(
     CodAtividade int NOT NULL,
     CodInstrutor int NOT NULL,
 );
+
 #----------------------------------------------
 #Criação das restrição das chaves estrageiras
 #----------------------------------------------
-ALTER TABLE Turma ADD CONSTRAINT FK_Cliente_CodAtividade (CodAtividade) REFERENCES Atividade (CodAtividade);
-ALTER TABLE Turma ADD CONSTRAINT FK_Instrutor_CodInstrutor (CodAtividade) REFERENCES Instrutor (CodInstutor);
+ALTER TABLE Turma ADD CONSTRAINT FOREIGN KEY foreing KEY FK_Cliente_CodAtividade (CodAtividade) REFERENCES Atividade (CodAtividade);
+ALTER TABLE Turma ADD CONSTRAINT FOREIGN KEY FK_Instrutor_CodInstrutor (CodAtividade) REFERENCES Instrutor (CodInstutor);
 
+#----------------------------------------------
+#Tabela Matricula
+#----------------------------------------------
+CREATE IF NOT EXISTS Matriculas(
+    CodMatricula INT NOT NULL,
+    CodTurma INT NOT NULL
+);
 
+#----------------------------------------------
+#FOREIGN KEY Tabela Matricula
+#----------------------------------------------
+ALTER TABLE Matricula ADD CONSTRAINT FOREIGN KEY FK_Aluno_CodMatricula (CodMatricula) REFERENCES Aluno (CodMatricula);
+ALTER TABLE Turma ADD CONSTRAINT FOREIGN KEY FK_Turma_CodTurma (CodTurma) REFERENCES Turma (CodTurma);
 
-
-
+#----------------------------------------------
+#Tabela Chamada
+#----------------------------------------------
+CREATE TABLE IF NOT EXISTS Chamada(
+    CodChamada INT NOT NULL,
+    Data DATE,
+    PRESENTE CHAR(1) #Valores S = Sim, N = Não
+    CodMatricula INT NOT NULL,
+    CodTurma INT NOT NULL,
+);
+#----------------------------------------------
+#RESTRIÇÃO DE CHAVE ESTRANGEIRA NA TABELA CHAMADA
+#----------------------------------------------
+ALTER TABLE Chamada ADD CONSTRAINT FOREIGN KEY FK_Matricula_CodMatricula (CodMatricula) REFERENCES Matricula (CodMatricula);
+ALTER TABLE Chamada ADD CONSTRAINT FOREIGN KEY FK_Matricula_CodTurma (CodTurma) REFERENCES Matricula (CodTurma);
